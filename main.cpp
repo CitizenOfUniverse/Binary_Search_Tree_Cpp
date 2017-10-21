0edf9b3a51e6b46f26bb9e9ff08843f270c8f4a5
CitizenOfUniverse <32209867+CitizenOfUniverse@users.noreply.github.com>
2017-10-21 13:17:03 +0300

    v1.01
    Added tree traverse by right-left-root method.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,19 @@
 #include "BST.h"
 
 int main(){
+	//Debug code
+	BST int_tree;
+	int_tree.insert(10);
+	int_tree.insert(15);
+	int_tree.insert(2);
+	int_tree.insert(8);
+	int_tree.insert(6);
+	int_tree.insert(9);
+	int_tree.insert(12);
+	int_tree.insert(13);
+	int_tree.insert(19);
+	int_tree.insert(18);
+	int_tree.insert(23);
+	int_tree.print_right_left_root();
 	return 0;
 }
